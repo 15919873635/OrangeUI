24352ca3f623aab55591264421a6acc47a225450
15919873635 <15919873635@126.com>
2018-12-16 16:29:35 +0800

    新增window组件

---
+++ component/window.vue
@@ -0,0 +1,21 @@
+<template>
+</template>
+<script>
+export default {
+    name: 'window',
+    data: function() {
+        return {}
+    },
+    props: {
+        
+    },
+    methods: {
+
+    }
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
+
+
