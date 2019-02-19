d3ed42b681a55e3d856c1d273a13b23a34f54b40
15919873635 <15919873635@126.com>
2019-02-19 23:06:26 +0800

    修改逻辑

--- js/extend/Clock.js
+++ js/extend/Clock.js
@@ -40,32 +40,22 @@
                     if(!clock.hasClass("clock"))
                         clock.addClass("clock");
                     var clockWidth = jQ.isNumeric(defaultOptions.width) ? defaultOptions.width : clock_default_width,
-                        clockHeight = jQ.isNumeric(defaultOptions.height) ? defaultOptions.height : clock_default_height,
-                        clockAlign = jQ.type(defaultOptions.align) === "string" ? defaultOptions.align : clock_default_align;
-                    clock.css({"width":clockWidth+"px","height":clockHeight+"px","float":clockAlign});
-                    if(clockAlign === clock_default_align)
-                        clock.removeClass("clock_align_right").addClass("clock_align_left");
-                    else
-                        clock.removeClass("clock_align_left").addClass("clock_align_right");
+                        clockHeight = jQ.isNumeric(defaultOptions.height) ? defaultOptions.height : clock_default_height;
+                    clock.css({"width":clockWidth+"px","height":clockHeight+"px"});
                     clock.empty();
                     var radius = 160;
-                    var dot_num = 360/$(".dot").length;
-                    var ahd = dot_num*Math.PI/180;
+                    var dot_num = 30;
+                    var ahd = dot_num * Math.PI / 180;
                     var clock_id = clock.prop("id");
-                    var initClockString ="<div class='origin'></div><div class='dot_box'>" + 
-                                        "<div class='clock_line hour_line' id='" + clock_id + "_hour_line'></div>" +
+                    var initClockString ="<div class='origin'></div><div class='dot_box'>";
+                    for(var index = 0 ; index < 12 ; index ++ ) {
+                        initClockString += "<div class='dot' style='left: " + (180 + Math.sin((ahd * index)) * radius) + "px; top: " + (180 + Math.cos((ahd * index)) * radius) + "px'>6</div>"
+                    }  
+                    initClockString += "</div><div class='clock_line hour_line' id='" + clock_id + "_hour_line'></div>" +
                                         "<div class='clock_line minute_line' id='" + clock_id + "_minute_line'></div>"+
                                         "<div class='clock_line second_line' id='" + clock_id + "_second_line'></div>" + 
                                         "<div class='date_info' id='" + clock_id + "_date_info'></div>" + 
                                         "<div class='time_info'><div class='time' id='" + clock_id + "_hour_time'></div><div class='time' id='" + clock_id + "_minute_time'></div><div class='time' id='" + clock_id + "_second_time'></div></div>";
-                    for(var index = 0 ; index < 12 ; index ++ ) {
-                        initClockString += "<div class='dot' style='left: " + (180 + Math.sin((ahd * index)) * radius) + "px; top: " + (180 + Math.cos((ahd * index)) * radius) + "px'>6</div>"
-                    }  
-                    initClockString += "</div><div class='clock_line hour_line' id='hour_line'></div>" +
-                                        "<div class='clock_line minute_line' id='minute_line'></div>"+
-                                        "<div class='clock_line second_line' id='second_line'></div>" + 
-                                        "<div class='date_info' id='date_info'></div>" + 
-                                        "<div class='time_info'><div class='time' id='hour_time'></div><div class='time' id='minute_time'></div><div class='time' id='second_time'></div></div>";
                     for(var i = 0; i < 60; i++) {
                         initClockString += "<div class='clock-scale' style='transform: rotate(" + (i * 6 - 90) + "deg);'><div class='scale-hidden'></div><div class='scale-show'></div></div>";
                     }                    
@@ -78,9 +68,7 @@
                     if(jQ.type(clockOptions) === "undefined"){
                         clockOptions = {
                             width:clock_default_width,
-                            align:clock_default_align,
-                            icon:null,
-                            text:clock_ok_text
+                            height:clock_default_height
                         };
                     }
                     if(jQ.isPlainObject(ops))
@@ -99,7 +87,7 @@
                     var nowdate = new Date();
                     //获取年月日时分秒
                     var year = nowdate.getFullYear(),
-                        month = nowdate.getMonth()+1,
+                        month = nowdate.getMonth() + 1,
                         day = nowdate.getDate(),
                         hours = nowdate.getHours(),
                         minutes = nowdate.getMinutes(),
