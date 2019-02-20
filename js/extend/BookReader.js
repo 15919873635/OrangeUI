9b614e1da4677bc54d794203367844ae3d57902f
15919873635 <15919873635@126.com>
2019-02-20 22:09:40 +0800

    1、新增BookReader的js实现 2、新增BookReader的css文件 3、新增PictureShear的css文件

--- js/extend/BookReader.js
+++ js/extend/BookReader.js
@@ -3,5 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+;(
+    function(jQ){
+        jQ.fn.extend({
+            OrangeBookReader:function(options){
+                
+            }
+        });
+    }
+)(jQuery);
 
 
